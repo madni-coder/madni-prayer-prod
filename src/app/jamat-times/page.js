7146155259df68376ce5e605bd4615b748ff03d7
Asad Madni <asadrazvi33@gmail.com>
2025-10-22 19:16:30 +0530

    dist

--- src/app/jamat-times/page.js
+++ src/app/jamat-times/page.js
@@ -472,7 +472,7 @@ export default function JamatTimesPage() {
 
             <div className="bg-white backdrop-blur-sm p-3 md:p-4 rounded-lg shadow-sm max-w-2xl mx-4 sm:mx-auto">
                 <h2 className="text-base md:text-lg font-extrabold text-primary text-center leading-tight">
-                    Currently this app shows Jamat Times of Bilaspur Zone (C.G)
+                    Currently this app shows Jamat Times of Bilaspur Dist (C.G)
                     Only
                 </h2>
             </div>
