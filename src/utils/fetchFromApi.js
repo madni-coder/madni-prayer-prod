fcb3e3c3b9324c438edb4f3e6e7012cbfb5a1ecb
Irfan Raza <irfaan.aa@gmail.com>
2026-01-24 03:46:09 +0530

    feat: Update iOS project configuration and API base URL.

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // fetchFromApi supports two signatures for convenience:
 // 1) fetchFromApi(endpoint, methodString = 'GET', payload)
